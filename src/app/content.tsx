2aca6858a0d104ad4779c048d831087503c20e7d
neoshock <pideun@gmail.com>
2025-08-27 10:06:34 -0500

    fix: update MARKETING_BOOST_PERCENTAGE to 15 for accurate sales calculations

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -28,7 +28,7 @@ import { TicketSearchModal } from "./components/TicketSearchModal";
 import { TicketOption } from "./types/tickets";
 import { TestimonialsSection } from "./components/TestimonialsSection";
 
-const MARKETING_BOOST_PERCENTAGE = 13;
+const MARKETING_BOOST_PERCENTAGE = 15;
 
 export default function HomeContent() {
   // Estados para modales
